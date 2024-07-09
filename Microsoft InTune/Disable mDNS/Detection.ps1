411927da9d4584f3fdc6d3ce890f670c75d762a1
Christopher Boni <mrcgboni@gmail.com>
2024-07-09 16:32:18 +0100

    Added comments to script.

--- Microsoft InTune/Disable mDNS/Detection.ps1
+++ Microsoft InTune/Disable mDNS/Detection.ps1
@@ -1,10 +1,15 @@
+# Get the value of the EnableMDNS regedit.
+# If entry doesn't exist, the $Reg variable will be null.
 $Reg = Get-ItemProperty -Path 'HKLM:\HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Dnscache\Parameters' -Name "EnableMDNS" -ErrorAction SilentlyContinue
-if ($Null -eq $Reg){
 
+# Check if entry was found.
+if ($Null -eq $Reg){
+    # Entry not found
     Write-host "mDNS regedit entry found"
     Exit 1
 }
 else {
+    # Entry found
     Write-Host "mDNS regedit entry not found"
     Exit 0
 }
