cffce0c1caed18aa97b0538a7d1c7c13461d3226
Christopher Boni <mrcgboni@gmail.com>
2024-07-16 11:53:29 +0100

    Added 60 minute timer before script stops.

--- Desktop Support/StayAwake.ps1
+++ Desktop Support/StayAwake.ps1
@@ -3,10 +3,10 @@
   Simple script that moves mouse cursor every 60 seconds to stop device from sleeping
 
 .NOTES
-  Version:        1.0
+  Version:        1.1
   Author:         Christopher Boni
   Creation Date:  16th July 2024
-  Purpose/Change: Initial script development
+  Purpose/Change: Added a 60 minute time limit.
 #>
 
 # Load the required libraries
@@ -16,8 +16,12 @@ Add-Type -AssemblyName System.Windows.Forms
 $x = [System.Windows.Forms.Cursor]::Position.X
 $y = [System.Windows.Forms.Cursor]::Position.Y
 
+# Start timer for 1 hour.
+$StartTime = Get-Date
+$EndTime - $StartTime.AddHours(1)
+
 # Start a loop to move mouse every 60 seconds.
-while ($true) {
+while ((Get-Date) -lt $EndTime) {
     # RNG new mouse position.
     $newX = $x + (Get-Random -Minimum -2 -Maximum 2)
     $newY = $y + (Get-Random -Minimum -2 -Maximum 2)
