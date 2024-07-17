fd990705d2e7582e2ba7118e9db70a17a299a74d
Christopher Boni <mrcgboni@gmail.com>
2024-07-17 11:38:13 +0100

    Fixed typos.

--- Desktop Support/StayAwake.ps1
+++ Desktop Support/StayAwake.ps1
@@ -3,10 +3,10 @@
   Simple script that moves mouse cursor every 60 seconds to stop device from sleeping
 
 .NOTES
-  Version:        1.3
+  Version:        1.3.1
   Author:         Christopher Boni
   Creation Date:  16th July 2024
-  Purpose/Change: Tweaked how the text is displayed.
+  Purpose/Change: Tweaked how the text is displayed. Fixed typos.
 #>
 
 # Load the required libraries
@@ -20,7 +20,7 @@ $y = [System.Windows.Forms.Cursor]::Position.Y
 $StartTime = Get-Date
 $EndTime = $StartTime.AddHours(1)
 
-# Adding some text so not an empty PS Window.
+# Clear PS Windows Screen and display text showing that script is running.
 Clear-Host
 Write-Host "Running script - mouse will move slightly every 60 seconds"
 Write-Host "Script will stop running after one hour, alternatively press CTRL + C to stop early"
@@ -31,7 +31,7 @@ while ((Get-Date) -lt $EndTime) {
     $newX = $x + (Get-Random -Minimum -2 -Maximum 2)
     $newY = $y + (Get-Random -Minimum -2 -Maximum 2)
     
-    # Move use to the RNG calc.
+    # Move mouse to the RNG calc.
     [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($newX, $newY)
 
     # Wait 60 seconds before going back to start of loop
