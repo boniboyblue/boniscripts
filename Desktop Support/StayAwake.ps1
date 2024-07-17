9a69976ac13d1f7cfece1d4855848811b5a53784
Christopher Boni <mrcgboni@gmail.com>
2024-07-17 18:08:22 +0100

    Reworked to simulate key press.

--- Desktop Support/StayAwake.ps1
+++ Desktop Support/StayAwake.ps1
@@ -1,16 +1,13 @@
 <#
 .DESCRIPTION
-  Simple script that moves mouse cursor every 60 seconds to stop device from sleeping
+Simple script that simulates a key press every 60 seconds to stop device from sleeping
 
 .NOTES
-  Version:        1.4
-  Author:         Christopher Boni
-  Creation Date:  16th July 2024
-  Purpose/Change: Reworked loop.
+Version:        1.5
+Author:         Christopher Boni
+Creation Date:  16th July 2024
+Purpose/Change: Reworked to simulate key press.
 #>
-
-# Load the required libraries
-Add-Type -AssemblyName System.Windows.Forms
 
 # Start timer for 1 hour.
 $StartTime = Get-Date
@@ -18,25 +15,14 @@ $EndTime = $StartTime.AddHours(1)
 
 # Clear PS Windows Screen and display text showing that script is running.
 Clear-Host
-Write-Host "Running script - mouse will move slightly every 60 seconds"
+Write-Host "Running script - Keeping device awake"
 Write-Host "Script will stop running after one hour, alternatively press CTRL + C to stop early"
 
-# Start a loop to move mouse every 60 seconds.
+# Start a loop to press key every 60 seconds.
+$KeyPress = New-Object -ComObject WScript.Shell
 while ((Get-Date) -lt $EndTime) {
-
-  # Find current mouse position
-  $x = [System.Windows.Forms.Cursor]::Position.X
-  $y = [System.Windows.Forms.Cursor]::Position.Y
-  
-  # RNG new mouse position.
-  $newX = $x + (Get-Random -Minimum -2 -Maximum 2)
-  $newY = $y + (Get-Random -Minimum -2 -Maximum 2)
-    
-  # Move mouse to the RNG calc.
-  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($newX, $newY)
-
-  # Line used for testing.
-  #Write-Host "Mouse Moved"
+  # Send F15 keyboard press
+  $KeyPress.SendKeys('+{F15}')
 
   # Wait 60 seconds before going back to start of loop
   Start-Sleep -Seconds 60
