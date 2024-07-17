cd724075f7ddda1e94305c1dc7275327128a746e
Christopher Boni <mrcgboni@gmail.com>
2024-07-17 17:42:18 +0100

    Reworked loop.

--- Desktop Support/StayAwake.ps1
+++ Desktop Support/StayAwake.ps1
@@ -3,18 +3,14 @@
   Simple script that moves mouse cursor every 60 seconds to stop device from sleeping
 
 .NOTES
-  Version:        1.3.1
+  Version:        1.4
   Author:         Christopher Boni
   Creation Date:  16th July 2024
-  Purpose/Change: Tweaked how the text is displayed. Fixed typos.
+  Purpose/Change: Reworked loop.
 #>
 
 # Load the required libraries
 Add-Type -AssemblyName System.Windows.Forms
-
-# Find current mouse position
-$x = [System.Windows.Forms.Cursor]::Position.X
-$y = [System.Windows.Forms.Cursor]::Position.Y
 
 # Start timer for 1 hour.
 $StartTime = Get-Date
@@ -27,13 +23,21 @@ Write-Host "Script will stop running after one hour, alternatively press CTRL + 
 
 # Start a loop to move mouse every 60 seconds.
 while ((Get-Date) -lt $EndTime) {
-    # RNG new mouse position.
-    $newX = $x + (Get-Random -Minimum -2 -Maximum 2)
-    $newY = $y + (Get-Random -Minimum -2 -Maximum 2)
-    
-    # Move mouse to the RNG calc.
-    [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($newX, $newY)
 
-    # Wait 60 seconds before going back to start of loop
-    Start-Sleep -Seconds 60
+  # Find current mouse position
+  $x = [System.Windows.Forms.Cursor]::Position.X
+  $y = [System.Windows.Forms.Cursor]::Position.Y
+  
+  # RNG new mouse position.
+  $newX = $x + (Get-Random -Minimum -2 -Maximum 2)
+  $newY = $y + (Get-Random -Minimum -2 -Maximum 2)
+    
+  # Move mouse to the RNG calc.
+  [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($newX, $newY)
+
+  # Line used for testing.
+  #Write-Host "Mouse Moved"
+
+  # Wait 60 seconds before going back to start of loop
+  Start-Sleep -Seconds 60
 }
